44341be7efb979e8489ef0b341235f6dba081d0c
yluom <leo.mouly@gmail.com>
2017-06-16 22:23:07 +0000

    Added new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import EventDateFilters from 'src/components/EventDateFilters';
 import ButtonDropdown from 'src/components/ButtonDropdown';
 import MobileEventFilters from 'src/components/MobileEventFilters';
 
-export default function(store){
+export default function(store) {
   return new Vue({
     name: 'header',
     store,
@@ -15,7 +15,9 @@ export default function(store){
         isFilterEventsOpen: false,
         // Where to position the expanded event filtering UI
         // for smaller screens
-        filterEventsTop: { top: 0 },
+        filterEventsTop: {
+          top: 0
+        },
       };
     },
     computed: {
@@ -36,12 +38,16 @@ export default function(store){
       toggleView() {
         store.commit('viewToggled');
       },
-      toggleFilterEvents: function() { this.isFilterEventsOpen = !this.isFilterEventsOpen },
+      toggleFilterEvents: function() {
+        this.isFilterEventsOpen = !this.isFilterEventsOpen
+      },
       search(e) {
         const newZipcode = e.target.value;
 
         if (/^\d{5}$/.test(newZipcode) || !newZipcode) {
-          store.dispatch('setFilters',{zipcode: newZipcode });
+          store.dispatch('setFilters', {
+            zipcode: newZipcode
+          });
         }
       }
     },
